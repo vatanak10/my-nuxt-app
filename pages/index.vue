2e8f5b06b24f08981dd90fce932749d77f42a83b
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-04-22 11:21:24 +0700

    :lipstick: add card

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <Tutorial />
+  <div class="home">
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
 </template>
 
+<style>
+  .home{
+    margin: 10px;
+  }
+
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+    width: 40%;
+    border-radius: 5px;
+    background-color: #f1f1f1;
+  }
+
+  .card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  .container {
+    padding: 2px 16px;
+  }
+</style>
 <script>
 export default {
   name: 'IndexPage',
+  data: () => ({
+    users: []
+  }),
+  async fetch() {
+    this.users = await this.$http.$get('https://api.nuxtjs.dev/posts')
+  },
 }
 </script>
