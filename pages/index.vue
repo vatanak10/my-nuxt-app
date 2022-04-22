23f093d77726eea57c639e440688e4386e97573c
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-04-22 11:23:28 +0700

    :pencil2: User

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <div class="home">
     <div class="card" style="width: 18rem;">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">Username</h5>
+        <h6 class="card-subtitle mb-2 text-muted">User email</h6>
+        <p class="card-text">User Address</p>
         <a href="#" class="card-link">Card link</a>
         <a href="#" class="card-link">Another link</a>
       </div>
